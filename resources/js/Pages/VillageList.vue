3eaf9ce394524584e8cb950268e695dc3d16fb42
Aleksey Mikheev <aleksvalmix@yandex.ru>
2023-04-28 12:10:48 +0500

    fix

--- resources/js/Pages/VillageList.vue
+++ resources/js/Pages/VillageList.vue
@@ -16,12 +16,11 @@ import { Page } from 'v-page'
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
 
-            <div v-for="(item) in $page.props.villages.data" class="bg-white overflow-hidden shadow-sm">
+            <div  v-for="(item) in $page.props.villages.data" style="align-items: center" class="py-5 px-2 d-flex align-items-center my- bg-white overflow-hidden shadow-sm">
                 <a :href="'village/'+item.id">
                     {{ item.name }}</a>
 
-
-                <hr>
+                <hr >
             </div>
 
             </div>
